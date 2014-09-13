3e6ee9d7b67402a98d1015840b932bbbfc040d90
yannickoo <drupal@yannickoo.de>
2014-09-13 21:30:06 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+  define('IMAGES_DIR', 'images/');
+
+  $files = scandir(IMAGES_DIR);
+  $files = array_slice($files, 2);
+
+  $background = array_rand($files);
+  $background = IMAGES_DIR . $files[$background];
+?>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style>
+      html,
+      body {
+        height: 100%;
+      }
+
+      body {
+        background-image: url(<?php print $background; ?>);
+        background-position: center center;
+        background-repeat: no-repeat;
+      }
+    </style>
+  </head>
+  <body>
+  </body>
+</html>
